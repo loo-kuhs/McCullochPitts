8292c83deb7430fafa24fa7a65101924f9e9be44
lucasB_py <upt.carlos.b.lucas.a@gmail.com>
2021-03-05 17:59:18 -0600

    Agregar archivos de proyecto.

---
+++ McCullochPitts/CullochPitts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McCullochPitts
+{
+    class CullochPitts
+    {
+        public int[,] x; //Valor de entrada
+        public int[] w; //Valor de
+        public int s,/*Valor de salida*/ umbral;
+
+        public CullochPitts(int numPatrones, int numEntradas, int umbral)
+        {
+            this.x = new int[numPatrones, numEntradas+1];
+            this.w = new int[numEntradas+1];
+            this.umbral = umbral;
+        }
+
+        public int Propagar(int patron)
+        {
+            int suma = 0;
+
+            for (int i = 1; i < w.Length; i++)
+            {
+                suma += x[patron, i] + w[i];
+            }
+
+            funcionActivacion(suma);
+            return s;
+        }
+
+        public void funcionActivacion(int suma)
+        {
+            if (suma > umbral)
+            {
+                s = 1;
+            }
+            else
+            {
+                s = 0;
+            }
+        }
+    }
+}
